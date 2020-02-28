f313c117e78df83582dbd3f1cda1acb31e5f027e
Aleksandr Kravets <a.kravets84@gmail.com>
2020-02-29 00:04:46 +0200

    update bot api lib with bot itself

---
+++ bot/bot.go
@@ -0,0 +1,164 @@
+package bot
+
+import (
+	"github.com/alexcom/tba/telegram"
+	"github.com/sirupsen/logrus"
+	"time"
+)
+
+type AuthZFunction func(update *telegram.Update) (allowed bool, chatID int, message string)
+
+type Options struct {
+	APIToken           string
+	Masters            []string
+	LongPollingTimeout int
+	FailRetryInterval  int
+	Authorized         AuthZFunction
+}
+
+// Constructor for Bot
+func NewBot(opts Options) (*Bot, error) {
+	mastersMap := map[string]bool{}
+	for _, master := range opts.Masters {
+		mastersMap[master] = true
+	}
+	status, err := LoadStatus()
+	if err != nil {
+		return nil, err
+	}
+	bot := Bot{
+		failRetryInterval: opts.FailRetryInterval,
+		Telegram:          telegram.NewClient(opts.APIToken, opts.LongPollingTimeout),
+		status:            *status,
+		authorized:        opts.Authorized,
+	}
+	return &bot, nil
+}
+
+type Bot struct {
+	authorized        AuthZFunction
+	failRetryInterval int
+	Telegram          *telegram.BaseClient
+	status            Status
+	updateHandlers    []UpdateHandler
+}
+
+func (bot Bot) Run() {
+	for {
+		updates, err := bot.Telegram.GetUpdates(telegram.GetUpdatesRequest{
+			Offset: bot.status.LastUpdate() + 1,
+		})
+		if err != nil {
+			logrus.WithError(err).Error("update receive failure")
+			time.Sleep(time.Duration(bot.failRetryInterval) * time.Second)
+			continue
+		}
+		for _, update := range *updates {
+			if allowed, chatID, message := bot.authorized(&update); allowed {
+				bot.processUpdate(&update)
+			} else {
+				bot.Report(chatID, WARN, message)
+			}
+			bot.status.SetUpdate(update.UpdateID)
+		}
+		if bot.status.Changed() {
+			if err = bot.status.Save(); err != nil {
+				logrus.WithError(err).Error("fail saving status file")
+			}
+		}
+	}
+}
+
+func (bot *Bot) processUpdate(update *telegram.Update) {
+
+	if len(bot.updateHandlers) == 0 {
+		logrus.Warn("no handlers registered. Please use OnX(...) methods to register some")
+		return
+	}
+
+	for _, handler := range bot.updateHandlers {
+		stop, err := handler(bot, update)
+		if err != nil {
+			logrus.WithError(err).Error("handling update")
+		}
+		if stop {
+			return
+		}
+	}
+}
+
+type UpdateHandler func(bot *Bot, update *telegram.Update) (breakChain bool, err error)
+type MessageHandler func(bot *Bot, update *telegram.Message) (breakChain bool, err error)
+type EditedMessageHandler func(bot *Bot, update *telegram.Message) (breakChain bool, err error)
+type ChannelPostHandler func(bot *Bot, update *telegram.Message) (breakChain bool, err error)
+type EditedChannelPostHandler func(bot *Bot, update *telegram.Message) (breakChain bool, err error)
+type InlineQueryHandler func(bot *Bot, update *telegram.InlineQuery) (breakChain bool, err error)
+type ChosenInlineResultHandler func(bot *Bot, update *telegram.ChosenInlineResult) (breakChain bool, err error)
+type CallbackQueryHandler func(bot *Bot, update *telegram.CallbackQuery) (breakChain bool, err error)
+type ShippingQueryHandler func(bot *Bot, update *telegram.ShippingQuery) (breakChain bool, err error)
+type PreCheckoutQueryHandler func(bot *Bot, update *telegram.PreCheckoutQuery) (breakChain bool, err error)
+type PollHandler func(bot *Bot, update *telegram.Poll) (breakChain bool, err error)
+
+func (bot *Bot) OnUpdate(handler UpdateHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, handler)
+}
+
+func (bot *Bot) OnMessage(handler MessageHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.Message)
+	})
+}
+
+func (bot *Bot) OnEditedMessage(handler MessageHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.Message)
+	})
+}
+
+func (bot *Bot) OnChannelPost(handler MessageHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.Message)
+	})
+}
+
+func (bot *Bot) EditedChannelPostMessage(handler MessageHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.Message)
+	})
+}
+
+func (bot *Bot) OnInlineQuery(handler InlineQueryHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.InlineQuery)
+	})
+}
+
+func (bot *Bot) OnChosenInlineResult(handler ChosenInlineResultHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.ChosenInlineResult)
+	})
+}
+
+func (bot *Bot) OnCallbackQuery(handler CallbackQueryHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.CallbackQuery)
+	})
+}
+
+func (bot *Bot) OnShippingQuery(handler ShippingQueryHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.ShippingQuery)
+	})
+}
+
+func (bot *Bot) OnPreCheckoutQuery(handler PreCheckoutQueryHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.PreCheckoutQuery)
+	})
+}
+
+func (bot *Bot) OnPoll(handler PollHandler) {
+	bot.updateHandlers = append(bot.updateHandlers, func(bot *Bot, update *telegram.Update) (breakChain bool, err error) {
+		return handler(bot, update.Poll)
+	})
+}
---
+++ bot/status.go
@@ -0,0 +1,81 @@
+package bot
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+type Status struct {
+	lastUpdate int
+	changed    bool
+}
+
+type jsonStatus struct {
+	LastUpdate int `json:"last_update"`
+}
+
+const statusFileName = "status.json"
+
+func LoadStatus() (*Status, error) {
+	file, err := os.Open(statusFileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			logrus.Info("no status file, will create new one")
+			return &Status{}, nil
+		}
+		return nil, err
+	}
+	decoder := json.NewDecoder(file)
+	jStatus := jsonStatus{}
+	err = decoder.Decode(&jStatus)
+	if err != nil {
+		return nil, err
+	}
+	logrus.Info("status reading success")
+	return &Status{
+		lastUpdate: jStatus.LastUpdate,
+	}, nil
+}
+
+func (s Status) Changed() bool {
+	return s.changed
+}
+
+func (s *Status) SetUpdate(update int) {
+	if s.lastUpdate == update {
+		return
+	}
+	if s.lastUpdate > update {
+		logrus.Warnf("new update number is %d, but last update is %d, ignoring\n", update, s.lastUpdate)
+		return
+	}
+	s.lastUpdate = update
+	s.changed = true
+}
+
+func (s Status) LastUpdate() int {
+	return s.lastUpdate
+}
+
+func (s *Status) Save() error {
+	if !s.changed {
+		return nil
+	}
+	file, err := os.OpenFile(statusFileName, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(&jsonStatus{LastUpdate: s.lastUpdate})
+	if err != nil {
+		return err
+	}
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	s.changed = false
+	logrus.Info("status saving success")
+	return nil
+}
