8e049b42e04dede2e06683471534204db8d618e9
alexcom <a.kravets84@gmail.com>
2021-04-04 16:43:56 +0300

    Cleanup bot API

--- bot/services.go
+++ bot/services.go
@@ -1,44 +1,8 @@
 package bot
 
 import (
-	"fmt"
 	"github.com/alexcom/tba/telegram"
-	"github.com/sirupsen/logrus"
 )
-
-type MessageLevel uint8
-
-// todo move to app
-const (
-	_ MessageLevel = iota
-	INFO
-	WARN
-	ERROR
-)
-
-func (bot Bot) Report(chatId int, level MessageLevel, msg string) {
-	req := telegram.SendMessageRequest{}
-	req.ChatID = chatId
-	req.Text = fmt.Sprintf("%s %s", icon(level), msg)
-	if _, err := bot.Telegram.SendMessage(req); err != nil {
-		logrus.WithError(err).Error("reporting")
-	}
-}
-
-// todo: move to app. not bot related
-func icon(level MessageLevel) string {
-	switch level {
-	case INFO:
-		return "ℹ"
-	case WARN:
-		return "⚠"
-	case ERROR:
-		return "‼"
-	default:
-		logrus.Errorf("unexpected Message Level %d", level)
-		return ""
-	}
-}
 
 type MessageServices interface {
 	SendText(chatID int, msg string) (*telegram.Message, error)
